225fc903aea2407d23b52d187b0da4b53fa3cdbf
DESKTOP-4QOSDC6\Mahadev <ajay.prajapati@sureprep.com>
2019-03-09 11:26:26 +0530

    Initial commit

---
+++ app/src/main/java/india/androidprac/customerlooperapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package india.androidprac.customerlooperapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
